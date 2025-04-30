74c23e7f0fe87131ef4009651c3f32bae020ff49
Munfa <munfa2014@gmail.com>
2025-05-01 00:30:51 +0600

    Add functions for the tree

--- Decision Tree/decision_tree.py
+++ Decision Tree/decision_tree.py
@@ -1,13 +1,28 @@
 import numpy as np;
+from collections import Counter
 
-def entropy():
+def entropy(y):
+    hist = np.bincount(y)
+    ps = hist / len(y)
+    return -np.sum([p * np.log(p) for p in ps if p>0])
+
+def information_gain(y, X_column, threshhold):
+    #parent entropy
+    parent_entropy = entropy(y)
+
+    left_node, right_node = split(X_column, threshhold)
+
+def split(X, thr):
     pass
 
-def information_gain():
-    pass
+def most_common_value(y):
+    counter = Counter(y)
+    value = counter.most_common(1)[0][0]
+    return value
 
-def best_split():
-    pass
+def best_split(X, y):
+    thr = None
+    gain = information_gain(y, X, thr)
 
 def build_tree():
     pass
