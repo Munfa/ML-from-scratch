a3dbea1a3edebc0f7ae684aea900cfd6d6c3b660
Munfa <munfa2014@gmail.com>
2025-06-06 23:20:09 +0600

    Updated calculation, added plot and comments

--- K-Means Clustering/k_means_clustering.py
+++ K-Means Clustering/k_means_clustering.py
@@ -14,40 +14,42 @@ def initialize_centroids(x, k):
 def update_centroids(X, y, k):
     centroids = np.zeros((k, X.shape[1])) # shape = k-rows and X-features
     for i in range(k):
-        cluster = X[y == i]
+        cluster = X[y == i]  # selecting the data belongs to each cluster
         if(len(cluster) > 0):
             centroids[i] = np.mean(cluster, axis=0) # getting the mean of a cluster as the new centroid
     
     return centroids
 
+# defining maximum iteration and tolerance for checking convergence
 def train_data(X, k, max_iter=200, tol=0.00001):
-    centroids = initialize_centroids(X, k)
-    print(centroids)
+    np.random.seed(123) # to get a consistent initial centroids and result
+    centroids = initialize_centroids(X, k) # getting random initial centroids
 
     for _ in range(max_iter):
-        y = [] # an empty list for clusters
+        clusters = [] # an empty list for clusters
 
         for datapoint in X:
             distances = eucleadian_distance(datapoint, centroids)
             cluster_num = np.argmin(distances)      # the datapoint belongs to this cluster
-            y.append(cluster_num)   
+            clusters.append(cluster_num)   
         
-        new_centroids = update_centroids(X, y, k)
+        clusters = np.array(clusters) # turning into numpy array to find the datapoints in each cluster
+        new_centroids = update_centroids(X, clusters, k) # updating centroids
         
-        centroids_change = eucleadian_distance(centroids, new_centroids)
+        centroids_change = eucleadian_distance(centroids, new_centroids) # calculating to check if centroids changed much
         if (centroids_change < tol).all():
             break   # we consider the centroids cannot be updated anymore
 
         centroids = new_centroids
 
-    return centroids, y
+    return centroids, clusters # returning centroids and clusters
         
 
-X, _ = datasets.make_blobs(n_samples=500, n_features=4, centers=5, random_state=123)
+# creating a toy dataset 
+X, _ = datasets.make_blobs(n_samples=500, n_features=2, centers=3, random_state=42)
 
-centroids, y = train_data(X, k=5)
-print(centroids)
+centroids, clusters = train_data(X, k=3)
 
-plt.scatter(X[:,0], X[:,1], c=y, cmap='viridis')
-plt.scatter(centroids[:, 0], centroids[:, 1], c='red', marker='X')
-# plt.show()
+plt.scatter(X[:,0], X[:,1], c=clusters, cmap='spring')
+plt.scatter(centroids[:, 0], centroids[:, 1], s=100, marker='+')
+plt.show()
